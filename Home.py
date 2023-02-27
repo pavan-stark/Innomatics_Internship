454f19818db3894b90bd0145282535e9086b7049
Pavan Kalyan <118463533+pavanvis@users.noreply.github.com>
2023-02-27 22:44:27 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -15,9 +15,9 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 # absolute path to this file's root directory
 PARENT_DIR = os.path.join(FILE_DIR,os.pardir)
 # absolute path of directory_of_interest
-dir_of_interest = os.path.join(PARENT_DIR,"resources")
+dir_of_interest = os.path.join(PARENT_DIR,"pavan")
 
-IMAGE_PATH = os.path.join(dir_of_interest,"images","photo.png")
+IMAGE_PATH = os.path.join(dir_of_interest,"pavan","photo.png")
 
 img = image.imread(IMAGE_PATH)
 st.image(img)
