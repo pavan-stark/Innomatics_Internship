d2e24ec1d86c2a50f2e9c382aa77842b300e2c7c
Pavan Kalyan <118463533+pavanvis@users.noreply.github.com>
2023-02-27 23:08:49 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,17 +10,7 @@ st.set_page_config(
 )
 st.title("Hi,I am Pavan Kalyan")
 
-# absolute path to this file
-FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-# absolute path to this file's root directory
-PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
-# absolute path of directory_of_interest
-dir_of_interest = os.path.join(PARENT_DIR, "pavan")
 
-IMAGE_PATH = os.path.join(dir_of_interest, "images", "photo.jpg")
-
-img = image.imread(IMAGE_PATH)
-st.image(img)
 
 st.subheader("I am a Data Science Enthusiast \nIntern at Innomatics Research Labs")
 
