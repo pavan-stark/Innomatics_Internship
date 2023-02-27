317673cb1992888780b74fd94ae02bf4fae6ef17
Pavan Kalyan <118463533+pavanvis@users.noreply.github.com>
2023-02-27 22:33:55 +0530

    Add files via upload

--- Home.py
+++ Home.py
@@ -10,6 +10,18 @@ st.set_page_config(
 )
 st.title("Hi,I am Pavan Kalyan")
 
+# absolute path to this file
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# absolute path to this file's root directory
+PARENT_DIR = os.path.join(FILE_DIR,os.pardir)
+# absolute path of directory_of_interest
+dir_of_interest = os.path.join(PARENT_DIR,"proj_1")
+
+IMAGE_PATH = os.path.join(dir_of_interest,"pavan","photo.png")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
 st.subheader("I am a Data Science Enthusiast \nIntern at Innomatics Research Labs")
 
 linkedin = 'https://www.linkedin.com/in/mekapothula-pavan-kalyan-294bb21b5/overlay/contact-info/'
--- pages/2_Dashboard_Titanic.py
+++ pages/2_Dashboard_Titanic.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from matplotlib import image
+import os
 import pandas as pd
 import plotly.express as px
-import os
+
+st.title("Dashboard - Titanic Data")
 
 # absolute path to this file
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,23 +13,19 @@ PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
 # absolute path of directory_of_interest
 dir_of_interest = os.path.join(PARENT_DIR, "resources")
 
-IMAGE_PATH = os.path.join(dir_of_interest, "images", "iris.jpg")
-DATA_PATH = os.path.join(dir_of_interest, "data", "iris.csv")
-
-st.title("Iris")
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "titanic.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "titanic.csv")
 
 img = image.imread(IMAGE_PATH)
 st.image(img)
-
 df = pd.read_csv(DATA_PATH)
 st.dataframe(df)
-
-species = st.selectbox("Select the Species:", df['Species'].unique())
+class1 = st.selectbox("Select the class:", df['class'].unique())
 
 col1, col2 = st.columns(2)
 
-fig_1 = px.histogram(df[df['Species'] == species], x="SepalLengthCm")
+fig_1 = px.histogram(df[df['class'] == class1], x="fare")
 col1.plotly_chart(fig_1, use_container_width=True)
 
-fig_2 = px.box(df[df['Species'] == species], y="SepalLengthCm")
+fig_2 = px.box(df[df['class'] == class1], y="fare")
 col2.plotly_chart(fig_2, use_container_width=True)
